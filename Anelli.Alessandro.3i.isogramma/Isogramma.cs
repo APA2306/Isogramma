04aedf91277d27261a4f7d2707cf4904b3e916bb
Alessandro Anelli <127589937+APA2306@users.noreply.github.com>
2023-05-12 12:02:32 +0200

    Add files via upload

---
+++ Anelli.Alessandro.3i.isogramma/Isogramma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+
+        
+        int lenght = word.Length;
+        string parola = word.ToLower();
+        char[] array = parola.ToCharArray();
+        int y;
+
+        for (int i = 0; i < lenght; i++)
+        {
+            y = i + 1;
+            while(y<lenght)
+            {
+                if(array[i] == array[y] && array[i] != ' ' &&  char.IsLetter(array[i]))
+                {
+                    return false;
+
+                }
+                y++;
+            }
+
+        }
+
+        return true;
+
+    }
+}
